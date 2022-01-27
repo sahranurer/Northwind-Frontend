c875f20531f9c630ddef74909b92861fa5a8b878
sahranurer <sahranurerr@gmail.com>
2022-01-27 16:00:12 +0300

    code refactoring-services

--- src/app/components/product/product.component.ts
+++ src/app/components/product/product.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ProductService } from 'src/app/services/product.service';
 import { Product } from '../models/product';
-import { HttpClient } from '@angular/common/http';
 import { ProductResponseModel } from '../models/productResponseModel';
 
 @Component({
@@ -10,20 +10,23 @@ import { ProductResponseModel } from '../models/productResponseModel';
 })
 export class ProductComponent implements OnInit {
   products: Product[] = [];
-  apiUrl = 'https://localhost:44392/api/products/getall';
+  dataLoaded = false;
+
   
 
-  constructor(private httpClient: HttpClient) {}
+  constructor(private productService:ProductService ) {}
 
   ngOnInit(): void {
     this.getProducts();
   }
 
-  getProducts() {
-    this.httpClient
-      .get<ProductResponseModel>(this.apiUrl)
-      .subscribe((response) => {
-        this.products = response.data
-      });
+  getProducts(){
+     this.productService.getProducts().subscribe(response=>{
+       this.products = response.data
+       this.dataLoaded = true;
+     });
+     
   }
+
+ 
 }
---
+++ src/app/services/product.service.ts
@@ -0,0 +1,21 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { ProductResponseModel } from '../components/models/productResponseModel';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProductService {
+
+  apiUrl = 'https://localhost:44392/api/products/getall';
+
+  constructor(private httpClient: HttpClient) { }
+
+  getProducts():Observable<ProductResponseModel> {
+    return this.httpClient
+      .get<ProductResponseModel>(this.apiUrl)
+      
+  }
+
+}
