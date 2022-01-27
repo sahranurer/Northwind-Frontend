8ae96f6aa5e544f7d9e8a1b2e30ae9ea2703c552
sahranurer <sahranurerr@gmail.com>
2022-01-27 14:52:24 +0300

    Products are listed with httpclient

--- src/app/components/product/product.component.ts
+++ src/app/components/product/product.component.ts
@@ -1,16 +1,29 @@
 import { Component, OnInit } from '@angular/core';
 import { Product } from '../models/product';
+import { HttpClient } from '@angular/common/http';
+import { ProductResponseModel } from '../models/productResponseModel';
 
 @Component({
   selector: 'app-product',
   templateUrl: './product.component.html',
-  styleUrls: ['./product.component.css']
+  styleUrls: ['./product.component.css'],
 })
 export class ProductComponent implements OnInit {
-  products:Product[] = [];
-  constructor() { }
+  products: Product[] = [];
+  apiUrl = 'https://localhost:44392/api/products/getall';
+  
+
+  constructor(private httpClient: HttpClient) {}
 
   ngOnInit(): void {
+    this.getProducts();
   }
 
+  getProducts() {
+    this.httpClient
+      .get<ProductResponseModel>(this.apiUrl)
+      .subscribe((response) => {
+        this.products = response.data
+      });
+  }
 }
